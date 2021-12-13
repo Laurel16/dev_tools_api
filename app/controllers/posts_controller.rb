625cf89ddc8e7891958aed12125dcee21b448247
laure leter <laure.leter@gmail.com>
2021-12-13 16:06:32 +0100

    show view to get post id api route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
+
+  before_action :set_params, only: [:index]
+
   def index
   @posts = Post.order('date ASC')
   render json: @posts
@@ -14,10 +18,27 @@ class PostsController < ApplicationController
     end
   end
 
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+
+
 private
   def post_params
     params[:post][:hashtag] ||= []
     params.require(:post).permit(:title, :date, :category, :content, :lead, :hashtag, :image)
   end
+
+  def set_params
+    @params = params
+    end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 
 end
