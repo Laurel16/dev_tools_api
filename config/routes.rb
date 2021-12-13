f7d6f0f176b09859a451ba6afb6808983eac70f2
laure leter <laure.leter@gmail.com>
2021-12-13 16:42:34 +0100

    change routes for categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'posts#index'
   get '/posts', to: 'posts#index'
   get '/categories', to: 'categories#index'
-  get '/category/Front Tools', to: 'pages#front'
-  get '/category/Nice Tutoriels', to: 'pages#nice'
-  get '/category/Web Culture', to: 'pages#web'
+  get '/categories/Front Tools', to: 'pages#front'
+  get '/categories/Nice Tutoriels', to: 'pages#nice'
+  get '/categories/Web Culture', to: 'pages#web'
 
   resources :posts
 
