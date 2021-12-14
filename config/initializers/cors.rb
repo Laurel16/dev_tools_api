ba0ff6a8e7c16a1e83465248c6e7057bb9b47d56
laure leter <laure.leter@gmail.com>
2021-12-14 18:20:50 +0100

    add devise

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,13 +3,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'http://localhost:3000'
     resource '*',
     headers: :any,
+    expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
     methods: [:get, :post, :patch, :put, :delete]
   end
 
-   allow do
-    origins 'https://mybesttools.herokuapp.com'
-    resource '*',
-    headers: :any,
-    methods: [:get, :post, :patch, :put, :delete]
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+
+
   root 'posts#index'
   get '/posts', to: 'posts#index'
   get '/categories', to: 'categories#index'
