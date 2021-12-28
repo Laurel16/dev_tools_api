faf9e678892776c59cd058a7276ba057cbdf9113
laure leter <laure.leter@gmail.com>
2021-12-28 17:48:46 +0100

    add front app address authorization in corbs.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,4 +7,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     methods: [:get, :post, :patch, :put, :delete]
   end
 
+   allow do
+    origins 'https://lauredevtools.herokuapp.com/'
+    resource '*',
+    headers: :any,
+    expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+    methods: [:get, :post, :patch, :put, :delete]
+  end
+
+
 end
